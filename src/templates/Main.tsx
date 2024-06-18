2708ef9451cd173376e5ab1ddaa6d1b5f9946b9e
matiferamador <107516034+matiferamador@users.noreply.github.com>
2024-06-18 15:36:37 -0300

    guardar cambiso

--- src/templates/Main.tsx
+++ src/templates/Main.tsx
@@ -17,20 +17,20 @@ const Main = (props: IMainProps) => (
     <div className="max-w-screen-md mx-auto">
       <div className="border-b border-gray-300">
         <div className="pt-16 pb-8">
+          <div className=" pb-8">
+            <Navbar>
+              <li className="mr-6">
+                <Link href="/">Inicio</Link>
+              </li>
+              <li className="mr-6">
+                <Link href="/about/">Sobre Mi</Link>
+              </li>
+            </Navbar>
+          </div>
           <div className="font-semibold text-3xl text-gray-900">
             {AppConfig.title}
           </div>
           <div className="text-xl">{AppConfig.description}</div>
-        </div>
-        <div>
-          <Navbar>
-            <li className="mr-6">
-              <Link href="/">Inicio</Link>
-            </li>
-            <li className="mr-6">
-              <Link href="/about/">Sobre Mi</Link>
-            </li>
-          </Navbar>
         </div>
       </div>
 
